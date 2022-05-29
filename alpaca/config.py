f03eb3ef6e1d98413699025cb2eda251b1e2059c
Jeffrey Zhou <jeffreyzhou@berkeley.edu>
2022-05-29 01:10:43 -0600

    Trader API

--- alpaca/config.py
+++ alpaca/config.py
@@ -1,4 +1,4 @@
 PAPER_API_key = "PK1RR1LENQM1R7JSCFMH"
-secret_key = "H4c0xPShZhk3M4QTQUoa4fRLJXrQmEU2RaV65BbV"
+Secret_key = "H4c0xPShZhk3M4QTQUoa4fRLJXrQmEU2RaV65BbV"
 
 PAPER_BASE_URL= "https://paper-api.alpaca.markets"
--- alpaca/trader.py
+++ alpaca/trader.py
@@ -3,3 +3,11 @@ from config import *
 from order import *
 
 APCA_API_BASE_URL= PAPER_BASE_URL
+ACCOUNT_URL = "{}/v2/account".format(APCA_API_BASE_URL)
+ORDER_URL = "{}/v2/orders".format(APCA_API_BASE_URL)
+HEADERS = {"APCA_API_KEY_ID": PAPER_API_key, "APCA_API_SECRET_KEY": Secret_key}
+
+def get_account():
+    r = requests.get(ACCOUNT_URL, headers=HEADERS)
+
+    return json.loads(r.content)
